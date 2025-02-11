aeb64e5ee8c83656c67c49a5b50bdb3961b5a324
frontDeveloper <gurbansahadowkemal01@gmail.com>
2025-02-11 17:38:12 +0300

    init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { JSX } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ru">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +15,4 @@ export default function Document() {
     </Html>
   );
 }
+
