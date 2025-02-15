b9f2309549e648f08a1b38f6c1760ac962fdf3fd
frontDeveloper <gurbansahadowkemal01@gmail.com>
2025-02-15 15:27:12 +0300

    сделал footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,5 +1,18 @@
+import cn from 'classnames';
+import { format } from 'date-fns';
+import styles from './Footer.module.css';
 import { FooterProps } from './Footer.props';
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <span>OwlTop &copy; 2020 - {format(new Date(), 'yyyy')} Все права защищены</span>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
