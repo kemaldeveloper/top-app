6579a35716ad542431927a6a9348df2cc209e072
frontDeveloper <gurbansahadowkemal01@gmail.com>
2025-02-15 14:41:33 +0300

    сделал разметку layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,18 +1,27 @@
-import { JSX } from 'react';
+import { FunctionComponent, JSX } from 'react';
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
+import styles from './Layout.module.css';
 import { LayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar/Sidebar';
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
+};
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
 };
